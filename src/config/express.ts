10270474b0e753b45abf91271448047086afe186
Nikolay Slavov <nikolay.slavov.96@gmail.com>
2025-03-30 10:11:44 +0300

    Attach authorization and authentication middlewares

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,7 +1,10 @@
 import { Express } from 'express'
 
+import { authorization } from '../middlewares';
+
 const config = (app: Express, express: any) => {
     app.use(express.json());
+    app.use(authorization());
 };
 
 export default config;
--- src/config/router.ts
+++ src/config/router.ts
@@ -1,9 +1,10 @@
 import { Express } from 'express'
 
 import { openAi, user } from "../router";
+import { isAuthenticated } from '../middlewares';
 
 const router = (app: Express) => {
-    app.use('/ai', openAi);
+    app.use('/ai', isAuthenticated(), openAi);
     app.use('/user', user);
 };
 
